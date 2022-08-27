656bf0a964ebe0e68c98026edadad9081d828217
Heidy <JackseniD@gmail.com>
2022-08-26 22:50:39 -0600

    Setting up vuex

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup</script>
 
 <template>
-  <div class="">
+  <div class="text-3xl">
     Initial commit
   </div>
 </template>
