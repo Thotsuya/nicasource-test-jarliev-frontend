da914ab489a5450ad72506422f0d70384e756bc8
Jarliev <jarliev2@gmail.com>
2022-08-28 01:22:18 -0600

    Podcasts section done, minor responsive fixes

--- src/components/Podcasts/Index.vue
+++ src/components/Podcasts/Index.vue
@@ -10,6 +10,12 @@
     <div v-if="podcasts" class="px-5 flex flex-col gap-8 mt-10">
       <Card v-for="podcast in podcasts" :key="podcast.id" :podcast="podcast"/>
     </div>
+    <div class="px-5 py-7 w-full flex flex-col items-center justify-content-center mt-6">
+      <button
+          class="md:w-60  w-full bg-transparent text-primary-red py-2 px-6 rounded border border-primary-red hover:bg-primary-red hover:text-white transition ease-in-out duration-300">
+        Show more episodes
+      </button>
+    </div>
   </div>
 </template>
 
